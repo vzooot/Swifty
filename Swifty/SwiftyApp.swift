06389979d57f111379caa189d17633f8fb6de8d4
vzotikas <vzotikas@hotmail.com>
2023-02-05 21:19:55 +0100

    Initial Commit

---
+++ Swifty/SwiftyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyApp.swift
+//  Swifty
+//
+//  Created by Administrator on 2/5/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
